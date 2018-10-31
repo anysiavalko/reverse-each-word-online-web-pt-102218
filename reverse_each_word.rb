690295415c48557895dfeaf0070cc5bb2dde3880
A. V <anysiavalko@gmail.com>
2018-10-31 14:56:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(string)
-  #we splt
+  
+  #we turn the string into an array with split method
   string_to_array = string.split(" ")
+  
+  #new empty array 
   new_array = []
   string_to_array.each do |string|
     new_array.push (string.reverse)
