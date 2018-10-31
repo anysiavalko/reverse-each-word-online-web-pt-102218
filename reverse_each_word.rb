d7e00eab825b40ca001845a3e72317e5379e9fe5
A. V <anysiavalko@gmail.com>
2018-10-31 15:02:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,4 +12,21 @@ def reverse_each_word(string)
   #Turn array back into a string
   new_array = new_array.join (" ")
 end
-    
+
+
+
+
+def reverse_each_word(string)
+  
+  #we turn the string into an array with split method
+  original_array = string.split(" ")
+  
+  #Make new empty array, then push each string from original array into it, in reverse
+  new_array = []
+  original_array.each do |string|
+    new_array.push (string.reverse)
+  end
+  
+  #Turn array back into a string
+  new_array = new_array.join (" ")
+end
