21a8178320df88e76290b1d9a4d4ef148be62dde
A. V <anysiavalko@gmail.com>
2018-10-31 14:53:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   string_to_array = string.split(" ")
   new_array = []
   string_to_array.each do |string|
-    string.reverse
+    new_array.push (string.reverse)
+    
   
     
