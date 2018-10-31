5fabf55a3f462b15864dc949a3f05cf9fa4d3dc5
A. V <anysiavalko@gmail.com>
2018-10-31 15:12:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ def reverse_each_word(string)
   end
   
   #Turn array back into a string
-  new_array = new_array.join (" ")
+  new_array.join (" ")
 end
 
 
@@ -28,7 +28,7 @@ def reverse_each_word(string)
   end
   
   #Turn array back into a string
-  new_array = new_array.join (" ")
+  new_array.join (" ")
 end
 
 
